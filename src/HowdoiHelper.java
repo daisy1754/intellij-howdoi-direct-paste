644fa2e05149040026578becfb21e9ba001c257e
daisy1754 <d-part@hotmail.co.jp>
2013-12-27 01:56:48 +0900

    Implement helper class to invoke howdoi.py script

---
+++ src/HowdoiHelper.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HowdoiHelper {
+    /**
+     * @return code snippet from howdoi.py script or null if something wrong happens.
+     */
+    public List<String> getCodeSnippet(String query) {
+        Runtime runtime = Runtime.getRuntime();
+        String path = this.getClass().getResource("howdoi.py").getPath();
+        try {
+            Process process = runtime.exec("python " + path + " " + query);
+            BufferedReader bufferedReader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+            List<String> lines = new ArrayList<String>();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+            }
+            bufferedReader.close();
+            return lines;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+}
--- src/HowdoiPasteAction.java
+++ src/HowdoiPasteAction.java
@@ -6,6 +6,8 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.LogicalPosition;
 import com.intellij.openapi.util.TextRange;
 
+import java.util.List;
+
 public class HowdoiPasteAction extends AnAction {
     public void actionPerformed(AnActionEvent actionEvent) {
         Editor editor = DataKeys.EDITOR.getData(actionEvent.getDataContext());
@@ -13,6 +15,29 @@ public class HowdoiPasteAction extends AnAction {
         LogicalPosition position = editor.getCaretModel().getLogicalPosition();
         int start = document.getLineStartOffset(position.line);
         String content = document.getText(new TextRange(start, start + position.column));
-        System.out.println(content);
+
+        List<String> snippet = new HowdoiHelper().getCodeSnippet(content);
+        if (snippet == null) {
+            // TODO: error handling
+        } else {
+            document.deleteString(start, start + position.column);
+            document.insertString(start, join(snippet, '\n'));
+            editor.getCaretModel().moveToLogicalPosition(
+                    new LogicalPosition(position.line + snippet.size() - 1, 0));
+        }
+    }
+
+    private String join(List<String> lines, char separator) {
+        StringBuilder builder = new StringBuilder();
+        boolean firstLine = true;
+        for (String line: lines) {
+            if (firstLine) {
+                firstLine = false;
+            } else {
+                builder.append(separator);
+            }
+            builder.append(line);
+        }
+        return builder.toString();
     }
 }
