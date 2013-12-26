971a63bbabc20219f3585430a3020ce0c5c4e464
daisy1754 <d-part@hotmail.co.jp>
2013-12-26 23:43:49 +0900

    Get text between current carret and start of the line

--- src/HowdoiPasteAction.java
+++ src/HowdoiPasteAction.java
@@ -1,9 +1,18 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.LogicalPosition;
+import com.intellij.openapi.util.TextRange;
 
 public class HowdoiPasteAction extends AnAction {
-    public void actionPerformed(AnActionEvent e) {
-        // TODO: insert action logic here
-
+    public void actionPerformed(AnActionEvent actionEvent) {
+        Editor editor = DataKeys.EDITOR.getData(actionEvent.getDataContext());
+        Document document = editor.getDocument();
+        LogicalPosition position = editor.getCaretModel().getLogicalPosition();
+        int start = document.getLineStartOffset(position.line);
+        String content = document.getText(new TextRange(start, start + position.column));
+        System.out.println(content);
     }
 }
