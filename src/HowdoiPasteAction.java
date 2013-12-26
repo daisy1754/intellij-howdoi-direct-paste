43e586100a7116b77b4f2c957c570a25d99f4e89
daisy1754 <d-part@hotmail.co.jp>
2013-12-26 23:15:06 +0900

    initial commit: autogenerated template

---
+++ src/HowdoiPasteAction.java
@@ -0,0 +1,9 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class HowdoiPasteAction extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+
+    }
+}
