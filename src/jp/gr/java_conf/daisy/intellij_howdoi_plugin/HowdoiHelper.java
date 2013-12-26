0ee7b465711a1959a422861c62a0a787cc081c04
daisy1754 <d-part@hotmail.co.jp>
2013-12-27 03:34:21 +0900

    Create temporary file from howdoi.py resource

--- src/jp/gr/java_conf/daisy/intellij_howdoi_plugin/HowdoiHelper.java
+++ src/jp/gr/java_conf/daisy/intellij_howdoi_plugin/HowdoiHelper.java
@@ -1,8 +1,10 @@
 package jp.gr.java_conf.daisy.intellij_howdoi_plugin;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,10 +13,10 @@ public class HowdoiHelper {
      * @return code snippet from howdoi.py script or null if something wrong happens.
      */
     public List<String> getCodeSnippet(String query) {
-        Runtime runtime = Runtime.getRuntime();
-        String path = this.getClass().getResource("/howdoi.py").getPath();
         try {
-            Process process = runtime.exec("python " + path + " " + query);
+            Runtime runtime = Runtime.getRuntime();
+            File tmpFile = createTmpFileFromResource("/howdoi.py");
+            Process process = runtime.exec("python " + tmpFile.getAbsolutePath() + " " + query);
             BufferedReader bufferedReader = new BufferedReader(
                     new InputStreamReader(process.getInputStream()));
             List<String> lines = new ArrayList<String>();
@@ -23,9 +25,28 @@ public class HowdoiHelper {
                 lines.add(line);
             }
             bufferedReader.close();
+            tmpFile.delete();
             return lines;
         } catch (IOException e) {
+            Notifications.Bus.notify(
+                    new Notification("HowdoiHelper", "HowdoiAction",
+                            "Unexpected error while getting snippet", NotificationType.ERROR)
+            );
             return null;
         }
+    }
+
+    private File createTmpFileFromResource(String resourcePath) throws IOException {
+        File tmpFile = new File("howdoi_helper_tmp.py");
+        InputStream stream = getClass().getResourceAsStream(resourcePath);
+        FileOutputStream out = new FileOutputStream(tmpFile);
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = stream.read(buffer)) != -1) {
+            out.write(buffer, 0, len);
+        }
+        stream.close();
+        out.close();
+        return tmpFile;
     }
 }
